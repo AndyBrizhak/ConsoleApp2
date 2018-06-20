337d702af9969d534f5b40e217df75609276c079
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-20 09:05:00 +0300

    +

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Del(int x, int y)
+        {
+            try
+            {
+                int result = x / y;
+            }
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("Деление на ноль!");
+            //}
+            // Данный блок будет всегда выполняться
+            finally
+            {
+                Console.WriteLine("Конец программы");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Del(5, 5);
+
+            Console.ReadLine();
+            Console.WriteLine("Ok ");
+            Console.ReadLine();
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp2
 {
@@ -10,6 +11,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            StreamWriter file = new StreamWriter("file.txt");
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                file.WriteLine("Error");
+                file.Close();
+            }
+            Console.ReadKey();
         }
     }
 }
